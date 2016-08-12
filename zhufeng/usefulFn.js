a72ccd4f2948eeb484e4977d15314e315cb26db2
ZhangShanTaurus <zhangshan_taurus@sina.cn>
2016-08-12 18:09:17 +0800

    getCss

--- zhufeng/usefulFn.js
+++ zhufeng/usefulFn.js
@@ -1,6 +1,6 @@
 var utils = {
   // 数组之间一一替换
-  zfindAndReplace:function zfindAndReplace(str, reg, oldarr, newarr) {
+  zfindAndReplace: function zfindAndReplace(str, reg, oldarr, newarr) {
       str = str.replace(reg, function() {
         try {
           return newarr[oldarr.indexOf(arguments[0])];
@@ -23,7 +23,7 @@ var utils = {
     var str = "ABCDADEC";
     console.log(findAndReplace(str, /\w/g, oldarr, newarr)) // abcdadec
      */
-  // 实现将类数组转化为数组
+    // 实现将类数组转化为数组
   zlistToArray: function(likeArray) {
     var arr = [];
     try {
@@ -36,13 +36,13 @@ var utils = {
     return arr;
   },
   // 把json格式的字符串变成json格式的对象
-  zjsonParse:function(str){
-  	// try{
-  	// 	return JSON.parse(str);
-  	// }catch(e){
-  	// 	return eval("("+str+")");
-  	// }
-  	return  "JSON" in window?JSON.parse(str):eval("("+str+")");
+  zjsonParse: function(str) {
+    // try{
+    //  return JSON.parse(str);
+    // }catch(e){
+    //  return eval("("+str+")");
+    // }
+    return "JSON" in window ? JSON.parse(str) : eval("(" + str + ")");
   },
   // 检测属性是不是公共属性
   zhasPublicProperty: function(obj, prop) {
@@ -80,60 +80,117 @@ var utils = {
 
 
 
-  // 检测属性是不是公共属性 
-  function zhasPublicProperty(obj, prop) {
-    // 首先必须是属于对象的属性 然后不是对象的私有属性
-    return (prop in obj) && !obj.hasOwnProperty(prop)
-  }
+// 检测属性是不是公共属性 
+function zhasPublicProperty(obj, prop) {
+  // 首先必须是属于对象的属性 然后不是对象的私有属性
+  return (prop in obj) && !obj.hasOwnProperty(prop)
+}
 
-  // 数组去掉重复的
-  // 思路:
-  // 1.建立一个空对象
-  // 2.从数组的第一个数开始,数组的项给对象的键值对,对象的键值相等
-  // 3.数组的每项和对象相应的键比较,如果对象有obj[23]=23;如在遇到数组有23 那么检测到obj[23]已经存在
+// 数组去掉重复的
+// 思路:
+// 1.建立一个空对象
+// 2.从数组的第一个数开始,数组的项给对象的键值对,对象的键值相等
+// 3.数组的每项和对象相应的键比较,如果对象有obj[23]=23;如在遇到数组有23 那么检测到obj[23]已经存在
 
-  function zunique(arr) {
-    var obj = {};
-    for (var i = 0, l = arr.length; i < l; i++) {
-      var cur = arr[i];
-      if (obj[cur] === cur) {
-        arr[i] = arr[l - 1];
-        arr.length--;
-        i--;
-      }
-      obj[cur] = cur;
+function zunique(arr) {
+  var obj = {};
+  for (var i = 0, l = arr.length; i < l; i++) {
+    var cur = arr[i];
+    if (obj[cur] === cur) {
+      arr[i] = arr[l - 1];
+      arr.length--;
+      i--;
     }
-    obj = null;
-    return arr;
+    obj[cur] = cur;
   }
+  obj = null;
+  return arr;
+}
 
-  // 检测类型
-  function ztypeof(para) {
-    return Object.prototype.toString.call(para).slice(8, -1).toLowerCase();
-  }
+// 检测类型
+function ztypeof(para) {
+  return Object.prototype.toString.call(para).slice(8, -1).toLowerCase();
+}
 
 // 时间字符串格式化  "2015-6-7 12:13:4".zformatTime() -> 2015年06月07日 12:14:04
 // 0,1,2,3,4,5分分表示年月日 时分秒
-  String.prototype.zformatTime = function zformatTime(formatStr){
-    var reg = /^(\d{4})[-/.](\d{1,2})[-/.](\d{1,2})\s+(\d{1,2}):(\d{1,2}):(\d{1,2})$/;
-    var arr = reg.exec(this).slice(1); // reg.exec(this)的最后的index和input不是数组的项
-    // arr = ["2015", "7", "9", "13", "9", "10"]
-    formatStr = formatStr || "{0}年{1}月{2}日 {3}:{4}:{5}";
-    formatStr = formatStr.replace(/{([\d]+)}/g,function(){
-      var val= arr[arguments[1]];
-      return val.length===1?"0"+val:val;
-    })
-    return formatStr;
+String.prototype.zformatTime = function zformatTime(formatStr) {
+  var reg = /^(\d{4})[-/.](\d{1,2})[-/.](\d{1,2})\s+(\d{1,2}):(\d{1,2}):(\d{1,2})$/;
+  var arr = reg.exec(this).slice(1); // reg.exec(this)的最后的index和input不是数组的项
+  // arr = ["2015", "7", "9", "13", "9", "10"]
+  formatStr = formatStr || "{0}年{1}月{2}日 {3}:{4}:{5}";
+  formatStr = formatStr.replace(/{([\d]+)}/g, function() {
+    var val = arr[arguments[1]];
+    return val.length === 1 ? "0" + val : val;
+  })
+  return formatStr;
+}
+
+
+// 返回浏览器的一屏幕的宽高 clientWidth
+// 返回浏览器的所有屏幕的宽高 scrollWidth
+// 
+function win(attr, value) {
+  if (typeof value === "undefined") {
+    return document.documentElement[attr] || document.body[attr];
   }
-  // 返回浏览器的一屏幕的宽高 clientWidth
-  // 返回浏览器的所有屏幕的宽高 scrollWidth
-  // 
-  function win(attr,value){
-    if(typeof value === "undefined" ){
-      return document.documentElement[attr] || document.body[attr]; 
+  document.documentElement[attr] = value;
+  document.body[attr] = value;
+
+
+}
+
+
+// 获取css的具体样式
+function getCss(curEle, attr) {
+  // if("getComputedStyle" in window){
+  if (window.getComputedStyle) {
+    // true表示存在
+    return window.getComputedStyle(curEle, null)[attr];
+
+  }
+  return curEle.currentStyle[attr];
+}
+// 精准检测不同浏览器 Detecting browsers by ducktyping:
+function detectNavigator() {
+  var obj = {
+    // Opera 8.0+
+    isOpera: (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0,
+    // Firefox 1.0+
+    isFirefox: typeof InstallTrigger !== 'undefined',
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+    isSafari: Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0,
+    // Internet Explorer 6-11
+    isIE: /*@cc_on!@*/ false || !!document.documentMode,
+    // Chrome 1+
+    isChrome: !!window.chrome && !!window.chrome.webstore,
+    // Edge 20+
+    isEdge: !this.isIE && !!window.StyleMedia
+      // isBlink:(this.isChrome || this.isOpera) && !!window.CSS
+  };
+}
+/*
+自己写的  不是很精准 因为userAgent可以改
+谷歌:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36      // Chrome/52.0.2743.116
+火狐:Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0     // Firefox/47.0
+IE:"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)"    // MSIE 10.0;
+safari:"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2"    //Safari/534.57.2
+opera:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48     // OPR/39.0.2256.48
+*/
+function getBrowser() {
+  var ua = navigator.userAgent;
+  // opera 里面有opera chrome safari
+  // chrome 里面有chrome safari
+  // safari 里面有safari
+  // 所以依次判断
+  var arr = /(MSIE)\s(\d+(.\d+)*)/.exec(ua) || /(Firefox)\/(\d+(.\d+)*)/.exec(ua) || /(OPR)\/(\d+(.\d+)*)$/.exec(ua) || /(Chrome)\/(\d+(.\d+)*)\s/.exec(ua) || /(Safari)\/(\d+(.\d+)*)$/.exec(ua);
+  if (arr !== null) {
+    return {
+      browser: arr[1],
+      version: arr[2]
     }
-    document.documentElement[attr] = value;
-    document.body[attr] = value;
-
-
+  } else {
+    return "浏览器不是ie firefox opera chrome safari"
   }
+}
+
