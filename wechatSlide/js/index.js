b2828366926e8ffe71be3230eb725f441944e8bb
frontzhm <1143303516@qq.com>
2016-11-09 23:49:44 +0800

    apply 才做一丢丢

---
+++ wechatSlide/js/index.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+	// autoplay: 5000,//可选选项，自动滑动
+	direction : 'vertical',
+	loop:true,
+	speed:500,
+	width : window.innerWidth,
+	height : window.innerHeight,
+	pagination : '.swiper-pagination',
+	// paginationType : 'progress',
+})
